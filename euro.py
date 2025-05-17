313351a27520063b0bf97337ef011f6a0ad09846
Michael Parker <michaelrbparker@protonmail.com>
2025-05-17 20:08:28 +0100

    unused port

--- euro.py
+++ euro.py
@@ -41,4 +41,4 @@ def results():
     return render_template('results.html', votes=dict(votes), predictions=dict(predictions))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=8595)
